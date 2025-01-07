8604e8e95cdf928975dc624f054d3b31f79679e7
송나리 <narsong@narsong.local>
2025-01-07 22:12:17 +0900

    middleware basic

--- server_test/server.js
+++ server_test/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-app.get('/', (res, req) => {
+
+const logMiddleware = (req, res, next) => {
+    console.log(req.method, res.url);
+    next();
+}
+
+app.get('/', logMiddleware, (req, res) => {
     res.status(200).send(req.params.id);
 });
 
+const errorMiddleware = (req, res, next) => {
+    next(new Error('미들웨어 에러'));
+}
+
+app.get('/err', errorMiddleware, (req, res) => {
+    console.log('err 라우트');
+    res.status(200).send('err 라우트');
+})
+
 app.use((err, req, res, next) => {
+    console.log(err);
     res.status(500).send('Internal Server Error');
 })
 
